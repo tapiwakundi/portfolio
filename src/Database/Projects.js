00aa3a84bfc673419f5a8862c6de9426cac145f3
Tapiwa Kundishora <tapiwakundi@gmail.com>
2020-11-01 18:37:16 -0700

    updated intuition page

--- src/Database/Projects.js
+++ src/Database/Projects.js
@@ -1,7 +1,7 @@
 import intuitionThumbnail from '../pages/images/int2.png'
 import savonaThumbnail from '../pages/images/savona-thumbnail.png'
 import teamappthumbnail from '../pages/images/team2.png'
-import intuitionMockup from '../pages/images/int-fs.jpg'
+import intuitionMockup from '../pages/images/int-fs.png'
 import savonaMockup from '../pages/images/savona.png'
 import teamappMockup from '../pages/images/team-fs.jpg'
 
@@ -10,7 +10,7 @@ export const Projects = [
         id: "intuition",
         name: "inTuition website",
         tags: ["Financial Markets", "Express", "Mongo DB"],
-        description: "Team App was a concept project for a team communication website. I made it through Webflow and took advantage of their powerful CMS platform to create a blog section that management can update on their timeline without a developer. This Application is fully responsive and highlights the foundational principles behind a reliable website for a growing company.",
+        description: "Intuition is an online global money transfer application for international students who want a faster, and more secure way to transfer money from their home nation to their school. inTuition is commited to developing inovative technologies that make the global financial markets more convinient for end users.",
         pageDescription: "Intuition is an online global money transfer application for international students who want a faster, and more secure way to transfer money from their home nation to their school. inTuition is commited to developing inovative technologies that make the global financial markets more convinient for end users.",
         thumbnail: intuitionThumbnail,
         mockup: intuitionMockup,
